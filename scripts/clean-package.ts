0172eb3c8b7599c8322c470e3cc1f0f54b90e288
Christian Schröter <dev@cschroeter.net>
2022-09-16 21:13:01 +0200

    chore: centralize config

--- scripts/clean-package.ts
+++ scripts/clean-package.ts
@@ -10,30 +10,10 @@ async function main() {
         ...pkg.manifest,
         scripts: {
           ...pkg.manifest.scripts,
-          prepack: "clean-package",
-          postpack: "clean-package restore",
         },
+        "clean-package": "../../../clean-package.config.json",
       }
 
-      const config = {
-        replace: {
-          main: "dist/index.cjs.js",
-          module: "dist/index.esm.js",
-          types: "dist/index.d.ts",
-          exports: {
-            ".": {
-              import: "./dist/index.esm.js",
-              require: "./dist/index.cjs.js",
-            },
-            "./package.json": "./package.json",
-          },
-        },
-      }
-
-      fs.writeFile(
-        `${pkg.dir}/clean-package.config.json`,
-        JSON.stringify(config, null, 2),
-      )
       fs.writeFile(`${pkg.dir}/package.json`, JSON.stringify(data, null, 2))
     }),
   )
